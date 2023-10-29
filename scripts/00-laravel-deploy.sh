0d106620c40db18b6384c1995e1834f1b851aa2b
Beloved46 <belovedakande@gmail.com>
2023-10-29 22:28:55 +0100

    install npm and yarn

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
@@ -8,6 +9,17 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+echo "Install Node.js and npm (specific version)..."
+# Modify the URL to specify the desired Node.js version (e.g., 14.x)
+curl -fsSL https://deb.nodesource.com/setup_20.5.0 | bash -
+apt-get install -y nodejs
+
+echo "Install yarn..."
+yarn install
+
+echo "Compress yarn build..."
+yarn build
 
 echo "Running migrations..."
 php artisan migrate --force
