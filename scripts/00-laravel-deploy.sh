f64063567102906823b98ad0c5f04802c3414043
Beloved46 <belovedakande@gmail.com>
2023-10-30 10:43:45 +0100

    route update

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,6 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "clear cache data"
-php artisan optimize:clear
 
 echo "Caching config..."
 php artisan config:cache
